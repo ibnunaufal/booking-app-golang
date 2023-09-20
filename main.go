8d6b3abb9ccd8e711d0e01ddb2e6fa95af166185
ibnu naufal <ibnunaufal5758@gmail.com>
2023-09-20 11:36:30 +0700

    using printf

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ func main() {
 	var eventName = "Go Conference"
 	const eventTickets = 50
 	var remainingTickets = eventTickets
-	fmt.Println("Welcome to " + eventName + ", Glad to see you!")
-	fmt.Println("We have total of ", eventTickets, " tickets with ", remainingTickets, " are still available.")
+	// fmt.Println("Welcome to " + eventName + ", Glad to see you!")
+	// fmt.Println("We have total of ", eventTickets, " tickets with ", remainingTickets, " are still available.")
+	fmt.Printf("Welcome to %v, Glad to see you!\n", eventName)
+	fmt.Printf("We have total of %v tickets with %v are still available.", eventTickets, remainingTickets)
 }
